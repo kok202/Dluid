cfdb22d48d5cf6e4a1a0c6cd183dc39fab51204a
kok202 <kok202@naver.com>
2020-01-16 03:41:57 +0900

    Channel 을 높이로 표현하려던 계획 철회

--- dluid-gui/src/main/java/org/kok202/dluid/application/content/design/component/AbstractLayerComponentController.java
+++ dluid-gui/src/main/java/org/kok202/dluid/application/content/design/component/AbstractLayerComponentController.java
@@ -2,7 +2,6 @@ package org.kok202.dluid.application.content.design.component;
 
 import javafx.beans.InvalidationListener;
 import javafx.scene.control.TextField;
-import org.kok202.dluid.CanvasConstant;
 import org.kok202.dluid.CanvasFacade;
 import org.kok202.dluid.ai.entity.Layer;
 
@@ -28,14 +27,9 @@ public abstract class AbstractLayerComponentController extends AbstractComponent
     }
 
     protected void notifyLayerDataChanged(){
-        double height = CanvasConstant.NODE_DEFAULT_HEIGHT;
-        if(layer.getProperties().isChannelExist()){
-            if(layer.getProperties().getOutputDimension() == 2)
-                height = CanvasConstant.NODE_DEFAULT_HEIGHT * layer.getProperties().getOutputSize()[1];
-            else if(layer.getProperties().getOutputDimension() == 3)
-                height = CanvasConstant.NODE_DEFAULT_HEIGHT * layer.getProperties().getOutputSize()[2];
-        }
-        CanvasFacade.findGraphNodeByLayerId(layer.getId()).getData().getBlockInfo().setHeight(height);
+        // Do we need to change the height value of the block node according to the channel?
+        // For reshape layers (when output dimension is 2D), we don't know that the y-values of output will be used as the depth of the block or as the height.
+        // Because y value can be used as channel or non channel.
         CanvasFacade.notifyLayerDataChanged(layer.getId());
     }
 }
