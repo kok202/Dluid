573f4666c721d5f9356cd2cf718041b396ddba0d
kok202 <kok202@naver.com>
2020-08-08 01:55:16 +0900

    set input output size bug on cnn layer

--- dluid-ai/src/main/java/org/kok202/dluid/ai/network/layer/applier/InputApplier.java
+++ dluid-ai/src/main/java/org/kok202/dluid/ai/network/layer/applier/InputApplier.java
@@ -1,5 +1,6 @@
 package org.kok202.dluid.ai.network.layer.applier;
 
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
 import org.deeplearning4j.nn.conf.layers.FeedForwardLayer;
 import org.deeplearning4j.nn.conf.layers.Layer.Builder;
 import org.kok202.dluid.domain.entity.LayerProperties;
@@ -8,12 +9,19 @@ public class InputApplier extends AbstractApplier {
 
     @Override
     public boolean support(Builder builder, LayerProperties layerProperties){
-        return builder instanceof FeedForwardLayer.Builder && layerProperties.getInputSize() != null;
+        return (builder instanceof FeedForwardLayer.Builder ||
+                builder instanceof ConvolutionLayer.BaseConvBuilder) &&
+                layerProperties.getInputSize() != null;
     }
 
     @Override
     public void apply(Builder builder, LayerProperties layerProperties){
-        FeedForwardLayer.Builder feedForwardLayerBuilder = ((FeedForwardLayer.Builder) builder);
-        feedForwardLayerBuilder.nIn(layerProperties.getInputVolume());
+        if(builder instanceof ConvolutionLayer.BaseConvBuilder){
+            ConvolutionLayer.BaseConvBuilder convolutionLayerBuilder = ((ConvolutionLayer.BaseConvBuilder) builder);
+            convolutionLayerBuilder.nIn(layerProperties.getInputSizeZ());
+        }else if(builder instanceof FeedForwardLayer.Builder){
+            FeedForwardLayer.Builder feedForwardLayerBuilder = ((FeedForwardLayer.Builder) builder);
+            feedForwardLayerBuilder.nIn(layerProperties.getInputVolume());
+        }
     }
 }
--- dluid-ai/src/main/java/org/kok202/dluid/ai/network/layer/applier/OutputApplier.java
+++ dluid-ai/src/main/java/org/kok202/dluid/ai/network/layer/applier/OutputApplier.java
@@ -1,5 +1,6 @@
 package org.kok202.dluid.ai.network.layer.applier;
 
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
 import org.deeplearning4j.nn.conf.layers.FeedForwardLayer;
 import org.deeplearning4j.nn.conf.layers.Layer.Builder;
 import org.kok202.dluid.domain.entity.LayerProperties;
@@ -8,13 +9,19 @@ public class OutputApplier extends AbstractApplier {
 
     @Override
     public boolean support(Builder builder, LayerProperties layerProperties){
-        return builder instanceof FeedForwardLayer.Builder && layerProperties.getOutputSize() != null;
+        return (builder instanceof FeedForwardLayer.Builder ||
+                builder instanceof ConvolutionLayer.BaseConvBuilder) &&
+                layerProperties.getInputSize() != null;
     }
 
     @Override
     public void apply(Builder builder, LayerProperties layerProperties){
-        FeedForwardLayer.Builder feedForwardLayerBuilder = ((FeedForwardLayer.Builder) builder);
-        feedForwardLayerBuilder.nOut(layerProperties.getOutputVolume());
+        if(builder instanceof ConvolutionLayer.BaseConvBuilder){
+            ConvolutionLayer.BaseConvBuilder convolutionLayerBuilder = ((ConvolutionLayer.BaseConvBuilder) builder);
+            convolutionLayerBuilder.nOut(layerProperties.getOutputSizeZ());
+        }else if(builder instanceof FeedForwardLayer.Builder){
+            FeedForwardLayer.Builder feedForwardLayerBuilder = ((FeedForwardLayer.Builder) builder);
+            feedForwardLayerBuilder.nOut(layerProperties.getOutputVolume());
+        }
     }
-
 }
